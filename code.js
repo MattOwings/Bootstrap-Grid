601a17ef3205e8910cdc104ce5d6a3a8f236ce46
MattOwings <MattAlanOwings@gmail.com>
2021-04-27 05:02:53 -0500

    initialized code.js

--- code.js
+++ code.js
@@ -2,3 +2,9 @@
 
 // DOM to emphasize a click on one of the example-elements
 // Will do soon
+
+// Plans:
+// If you click one of the example elements it switches color and does a simple animation
+// purpose: the idea is for the elements to be easy to read and easy to click 
+// no matter what your screen size is, therefore showing emphasis for a click is
+// important to display. 
